3e1a2456eb566d9e4c3ce82b09e5f95a279904e6
tabeeb choudhury <c17350031@mydit.ie>
2018-11-01 14:53:51 +0000

    the project so far

---
+++ Assets/collisions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisions : MonoBehaviour {
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = gameObject.GetComponent<Animator>();
+	}
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("fists"))
+        {
+            anim.SetBool("gothit", true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/dam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class dam : MonoBehaviour {
+    Animator anim;
+    public static int health=100;
+    public Slider healthbar;
+    private void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    public void damage()
+    {
+        health -= 5;
+        anim.SetBool("gothit", false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        healthbar.value = health;
+	}
+}
